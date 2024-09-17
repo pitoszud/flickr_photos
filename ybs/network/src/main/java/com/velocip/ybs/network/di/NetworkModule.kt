71befc6fb379fd4c530d36c17ba80d5799e26b5c
Patryk Jakubik <13464778+pitoszud@users.noreply.github.com>
2024-09-17 20:46:02 +0100

    build dependency injection modules

---
+++ ybs/network/src/main/java/com/velocip/ybs/network/di/NetworkModule.kt
@@ -0,0 +1,55 @@
+package com.velocip.ybs.network.di
+
+import com.velocip.ybs.network.utils.UrlProvider
+import com.velocip.ybs.network.utils.UrlProviderInterface
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideKtorHttpClient(): HttpClient {
+        return HttpClient(Android) {
+            install(HttpTimeout) {
+                requestTimeoutMillis = TimeUnit.SECONDS.toMillis(20)
+                socketTimeoutMillis = TimeUnit.SECONDS.toMillis(20)
+                connectTimeoutMillis = TimeUnit.SECONDS.toMillis(20)
+            }
+
+            install(ContentNegotiation) {
+                json()
+            }
+
+            install(DefaultRequest) {
+                headers.append("Content-Type", "application/json")
+            }
+
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.ALL
+            }
+        }
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideUrlProvider(): UrlProviderInterface = UrlProvider()
+}
---
+++ ybs/network/src/main/java/com/velocip/ybs/network/di/RemoteDataSourceModule.kt
@@ -0,0 +1,20 @@
+package com.velocip.ybs.network.di
+
+import com.velocip.ybs.network.utils.PhotoItemBuilder
+import com.velocip.ybs.network.utils.PhotoItemBuilderImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RemoteDataSourceModule {
+
+    @Binds
+    internal abstract fun bindPhotoBuilder(impl: PhotoItemBuilderImpl): PhotoItemBuilder
+
+    companion object {
+
+    }
+}
